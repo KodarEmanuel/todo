b6a4f9d2564bed51ce5e20fe4875587369c50237
wilari932 <33053518+wilari932@users.noreply.github.com>
2018-09-24 09:25:31 +0200

    ändringar
    ändringar

--- ToDoMain.js
+++ ToDoMain.js
@@ -1,19 +1,45 @@
+
+var datalist = [];
+let listOfTodos =  document.getElementById("ul-list");
+
+document.querySelector("form").addEventListener("submit",function(e){
+    e.preventDefault();
+    AddTask();
+    return false;
+});
+
 function AddTask()
 {
-    if(document.getElementById("new-todo").value == "")return;
-    var node = document.createElement("LI");
-    var textnode = document.createTextNode(document.getElementById("new-todo").value);
-    node.appendChild(textnode);
-    document.getElementById("ul-list").appendChild(node);
-    document.getElementById("new-todo").value = "";
+
+    let input = document.querySelector("form input");
+    let node1 = document.createElement("li");
+    let node2 = document.createElement("div");
+    let node3 = document.createElement("h2");
+    let node4 = document.createElement("button");
+    node4.className = "btn-green";
+    node4.textContent ="Delete";
+    
+    node2.appendChild(node3);
+    node2.appendChild(node4);
+    node1.appendChild(node2);
+
+   node3.innerHTML  = input.value;
+   listOfTodos.appendChild(node1);
+   
+  
 }
 
-function lowerCase  (e) {
-    if (e.keyCode == 13) {
-       AddTask();
-    }        
-}
 
+// // function lowerCase  (e) {
+    
+
+    
+
+
+// //     if (e.keyCode == 13) {
+// //        AddTask();
+// //     }        
+// }
 
 /*
   function function1() {
