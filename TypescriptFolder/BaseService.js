995240309a0e81733137eeb8d128f2bd14391706
TradeArn <mail@EmanuelJohansson.se>
2018-10-08 08:57:00 +0200

    lo

--- TypescriptFolder/BaseService.js
+++ TypescriptFolder/BaseService.js
@@ -1,20 +1,11 @@
-var TodoApp;
-(function (TodoApp) {
-    var BaseService;
-    (function (BaseService_1) {
-        var BaseService = /** @class */ (function () {
-            function BaseService() {
-            }
-<<<<<<< HEAD
-            BaseService.prototype.test = function () {
-                alert("HELLO WORLD");
-=======
-            IBaseService.prototype.test = function () {
-                alert("HELLO WORLD test");
->>>>>>> f28f86c86d2fd9928c195278282d977f2a570852
-            };
-            return BaseService;
-        }());
-        BaseService_1.BaseService = BaseService;
-    })(BaseService = TodoApp.BaseService || (TodoApp.BaseService = {}));
-})(TodoApp || (TodoApp = {}));
+var test;
+(function (test) {
+    class foo {
+        get bar() {
+            return this._bar;
+        }
+        set bar(theBar) {
+            this._bar = theBar;
+        }
+    }
+})(test || (test = {}));
--- TypescriptFolder/BaseService.ts
+++ TypescriptFolder/BaseService.ts
@@ -1,15 +1,5 @@
-namespace TodoApp.BaseService {
+namespace test
+{
 
-  export  interface IBaseService{
-   test():void;
-  }
-
-  export  class BaseService implements IBaseService{
-
-   public test():void {
-        alert("HELLO WORLD test");
-    }
-      
+   
 }
-    
-    }
